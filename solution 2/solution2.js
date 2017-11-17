2fca3964d1ab8b8b968057fd6f2291effa94752f
jacob <worisj@gmail.com>
2017-11-17 08:04:50 -0800

    Just another commit

--- solution 2/solution2.js
+++ solution 2/solution2.js
@@ -1,14 +1,24 @@
 var fs = require('fs');
 var word = fs.readFileSync("Problem set 2.txt");
-var arr = word.toString().split("\n");
-for(i = 0; i<10; i++){
+word = word.toString().replace(/\r?\n|\r/g, " ");
+var arr = word.toString().split(" ");
+for(i = 1; i<arr.length; i++){
     newarr = arr[i].toString().split('');
-    nxtarr = newarr[i+1];
-    for(j = 0; j<newarr.length; i++){
-        nxtarr.forEach(function(element) {
-            
-        };
+    nxtarr = arr[i+1].toString().split('');
+    var x = newarr.join("");
+       newarr.forEach(function(element) {
+            var inarr = nxtarr.filter(function(word){
+                return word == element;
+            });
+    var y = inarr.join("");
+    if(x == y){
+        console.log("Match found");
+    }else{
+        console.log("No match found");
     }
+            
+        });
+
 
 }
 
